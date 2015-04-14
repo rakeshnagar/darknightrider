7699a0b92e5b55d31786db6eb224d9e1438e48ef
rakeshnagar <rakeshnagar@hotmail.com>
2015-04-13 20:35:23 -0400

    server side authorization added

--- MultiVision/public/app/app.js
+++ MultiVision/public/app/app.js
@@ -14,7 +14,30 @@ angular.module('app').config(function ($routeProvider, $locationProvider) {
         requireBase: false
     });
 
-    $routeProvider.when('/', {templateUrl: '/partials/main/main', controller: 'mvMainCtrl'})
-    .when('/admin/users', {templateUrl: '/partials/admin/user-list', controller: 'mvUserListCtrl'});
-});
+    $routeProvider
+        .when('/', {templateUrl: '/partials/main/main', controller: 'mvMainCtrl'})
+        .when('/admin/users', {templateUrl: '/partials/admin/user-list',
+            controller: 'mvUserListCtrl',
+            resolve: {
+                /* only allow admin uses to access this link */
+                auth: function (mvIdentity, $q) {
+                    if (mvIdentity.currentUser && mvIdentity.currentUser.roles.indexOf('admin') > -1) {
+                        return true;
+                    } else {
+                        return $q.reject('not authorized');
+                    }
+                }
+            }
+        });
+})
+;
 
+angular.module('app').run(function ($rootScope, $location) {
+    $rootScope.$on('$routeChangeError', function (evt, current, previous, rejection) {
+
+        if (rejection === 'not authorized') {
+            $location.path('/');
+            /* redirect to home page*/
+        }
+    });
+});
