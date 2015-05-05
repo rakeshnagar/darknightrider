b3f68dea0460c4d4b341387aa711637300f56808
rakeshnagar <rakeshnagar@hotmail.com>
2015-05-04 23:09:05 -0400

    update user profile functionality added

--- MultiVision/public/app/app.js
+++ MultiVision/public/app/app.js
@@ -7,6 +7,11 @@ angular.module('app').config(function ($routeProvider, $locationProvider) {
             auth: function (mvAuth) {
                 return mvAuth.authorizeCurrentUserForRoute('admin');
             }
+        },
+        user: {
+            auth: function (mvAuth) {
+                return mvAuth.authorizeAuthenticatedUserForRoute();
+            }
         }
     };
 
@@ -31,7 +36,9 @@ angular.module('app').config(function ($routeProvider, $locationProvider) {
             controller: 'mvUserListCtrl', resolve: routeRoleChecks.admin
         })
         .when('/signup', {templateUrl: '/partials/account/signup',
-            controller: 'mvSignupCtrl'});
+            controller: 'mvSignupCtrl'})
+        .when('/profile', {templateUrl: '/partials/account/profile',
+            controller: 'mvProfileCtrl', resolve: routeRoleChecks.user});
 });
 
 angular.module('app').run(function ($rootScope, $location) {
